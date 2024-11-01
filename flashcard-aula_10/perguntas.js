cb2a78160e387dd60ea3776023ed222dbfd16bd0
sarahhsouza <161044212+sarahhsouza@users.noreply.github.com>
2024-11-01 10:48:28 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o rio mais longo do Reino Unido ?',
+    'Rio Severn'
+)
+
+criaCartao(
+    'Artes',
+    'Quem pintou a famoa obra "Noite Estrelada"?',
+    'Vicent Van Gogh'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
